cea0b99cfc71d03a109aaeb3895a3d593bd7c8f2
Surya Pratap Singh Rathore <rathoresurya21@gmail.com>
2021-08-07 11:59:08 +0530

    minor changes

--- MarutiAcademy/urls.py
+++ MarutiAcademy/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 from django.urls.conf import include
 urlpatterns = [
     path('', include('home.urls')),
-    path('admin/', admin.site.urls),
+    path('marvel/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
